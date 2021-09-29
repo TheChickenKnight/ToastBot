3e5b10fcddc4225124be66a79f9c66a0f4c40e80
TheChickenKnight <chcknknight@gmail.com>
2021-09-29 08:20:04 -0400

    Update mcstatus.js

--- commands/utility/mcstatus.js
+++ commands/utility/mcstatus.js
@@ -17,7 +17,6 @@ module.exports.run = async (client, message, args) => {
             var file = new MessageAttachment('./Images/DefaultMinecraft.png');;
             if (res.ip == "127.0.0.1")return message.reply('That doesn\'t seem to be a valid server. Perhaps you spelled it wrong?')
             else {
-                var embed = new MessageEmbed().setTitle((res.hostname || `${res.ip}:${res.port}`) + (!(!res.version) ? 'yes' : 'no')).setColor("RED").setDescription("❌ This server is currently offline!").setThumbnail('attachment://DefaultMinecraft.png');
                 if (res.online) {
                     file = new MessageAttachment(new Buffer.from(res.icon.split(',')[1], 'base64'), 'img.png');
                     embed.setThumbnail('attachment://img.png').setColor(client.randToastColor()).setDescription(`**${res.motd.clean[Math.floor(Math.random() * res.motd.clean.length)]}**\n\`${res.players.online}\` out of \`${res.players.max}\` players are currently playing!`)

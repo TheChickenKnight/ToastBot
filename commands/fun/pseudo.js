8c01f1c466328c48aa1513425441d862473b882a
TheChickenKnight <chcknknight@gmail.com>
2021-10-11 23:15:59 -0400

    Update pseudo.js

--- commands/fun/pseudo.js
+++ commands/fun/pseudo.js
@@ -16,7 +16,6 @@ module.exports.run = async (client, message, args) => {
     var author, text;
     if (message.mentions.members.first())author = await client.users.fetch(message.mentions.members.first().id), text = args.slice(1).join(" ")
     else author = message.author, text = args.join(" ");
-    console.log()
-    await webhooks[0][1].send({ content: text, username:  await message.guild.members.cache.get(author.id).nickname, avatarURL: author.displayAvatarURL({format: 'png'})});
+    await webhooks[0][1].send({ content: text, username: message.guild.members.cache.get(author.id).nickname || author.username, avatarURL: author.displayAvatarURL({format: 'png'})});
 }
 

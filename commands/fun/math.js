f579b54d3bb46d12ded7fdab5e8845c392a4752b
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2021-11-22 21:34:40 -0500

    math-upd

--- commands/fun/math.js
+++ commands/fun/math.js
@@ -11,19 +11,20 @@ const Dannjs = require('dannjs');
 const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
-    if (args.length !== 3)return message.reply('Wrong args!');
+    if (args.length !== 3 || args[1] != '+')return message.reply('Wrong args!');
     var num;
     if (!(await client.redis.EXISTS('neural_number'))) {
-        num = new Dannjs.dann(3, 1);
-        num.addHiddenLayer(5, 'sigmoid');
-        num.addHiddenLayer(3, 'sigmoid');
+        num = new Dannjs.dann(2, 1);
+        num.addHiddenLayer(4, 'sigmoid');
+        num.addHiddenLayer(2, 'sigmoid');
         num.makeWeights();
     } else num = Dann.createFromJSON(JSON.parse(await client.redis.get('neural_number')));
-    args[1] = ['+', '-', '*', '/'].indexOf(args[1]);
+    args.splice(1, 1);
     args = args.map(arg => parseInt(arg));
+    if (args[0] + args[1] > 10)return message.reply('Numbers TOO BIG!!!!')
     const answer = num.feedForward(args);
-    const filter = m => /[0-9]+/.test(m.content);
-    const msg = await message.reply({embeds: [new MessageEmbed().setColor(client.randToastColor()).setDescription('I got ' + answer*100 + '. What\'s the real answer?').setFooter('I\'ll take an answer from anyone!')]});
+    const filter = m => /[0-9]+/.test(m.content) && parseInt(m.content) <= 10;
+    const msg = await message.reply({embeds: [new MessageEmbed().setColor(client.randToastColor()).setDescription('I got ' + answer*10 + '. What\'s the real answer?').setFooter('I\'ll take an answer from anyone!')]});
     message.channel.awaitMessages({ filter, time: 60000, max: 1, errors: ['time']}).then(async messages => {
         num.backpropagate(args, [parseInt(messages.first().content)]);
         await client.redis.set('neural_number', JSON.stringify(num.toJSON()));

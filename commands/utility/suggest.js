597f719ec2198b3a8c0f4d62e557bb149dd57fa3
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2021-12-15 23:03:44 -0500

    a

--- commands/utility/suggest.js
+++ commands/utility/suggest.js
@@ -13,10 +13,12 @@ module.exports.run = async (client, message, args) => {
     if (!args[0])return message.reply('You need to suggest something!');
     let embed = new MessageEmbed().setColor(client.randToastColor()).setTitle(args.map(arg => arg.charAt(0).toUpperCase() + arg.slice(1).toLowerCase()).join(' '));
     let suggestions = [message.author.id];
+    let suggestors = await client.redis.KEYS('suggestion_*');
+    suggestors.filter(s => similar(s.split('_')[1], args.join(' ').toLowerCase()) > 2).sort((a, b) => a > b);
     if (await client.redis.EXISTS('suggestion_' + args.join(' ').toLowerCase())) {
         suggestions = await client.redis.SMEMBERS('suggestion_' + args.join(' ').toLowerCase());
         embed.setFooter('This suggestion exists already!');
-    }
+    } else if (suggestors.length > 0)embed.addField('Similar', 'Similar to the `' + suggestors[0] + '`');
     await client.redis.SADD('suggestion_' + args.join(' ').toLowerCase(), message.author.id);
     message.reply({
         embeds: [embed.setDescription('`' + suggestions.length + '/3`\n'+ client.barCreate(suggestions.length * 100 / 3))],
@@ -43,3 +45,5 @@ module.exports.button = async (client, interaction) => {
         });
     }
 };
+
+const similar = (text1, text2) => text1.split(' ').filter(word => word.length > 3 && text2.split(' ').includes(word)).length;

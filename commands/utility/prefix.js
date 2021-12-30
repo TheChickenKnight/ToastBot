943f22bbf7cecd3cd6d70b06d7f669baabd51725
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2021-12-30 16:08:23 -0500

    prefix fix

--- commands/utility/prefix.js
+++ commands/utility/prefix.js
@@ -9,14 +9,14 @@ module.exports.info = {
 const { MessageEmbed } = require('discord.js');
 
 module.exports.run = async (client, message, args) => {
-    const prefix = await client.redis.get(`guildSpec.${message.guildId}.prefix`);
+    const prefix = await client.redis.HGET(`guildSpec_${message.guildId}`, 'prefix');
     var embed = new MessageEmbed().setColor('#ff0000');
     const newPrefix = args.join(' ').replace(/"/g, '');
     if (!args[0])embed.setDescription(`The current prefix is \`${prefix}\`!`).setFooter('You can also ping me for this information!').setColor(client.randToastColor());
     else if (newPrefix.length > 6)embed.setDescription('A prefix cannot be longer than 6 letters!');
     else if (newPrefix == prefix)embed.setDescription('That\'s already the current prefix!');
     else {
-        await client.redis.set(`guildSpec.${message.guildId}.prefix`, newPrefix);
+        await client.redis.HSET(`guildSpec_${message.guildId}`, 'prefix', newPrefix);
         embed.setDescription(`Got it, the prefix(in this server) has been changed to \`${newPrefix}\`!`).setColor(client.randToastColor()).setFooter('Tip: if something isn\'t working right with the prefix, surround it with quotes(") first!');
     } message.reply({ embeds: [embed]});
 }

1855e1fb74c5b1cae4e94e388478a4e4ebd7c338
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2021-12-30 16:01:04 -0500

    default mc server

--- commands/minecraft/mcstatus.js
+++ commands/minecraft/mcstatus.js
@@ -7,13 +7,13 @@ module.exports.info = {
     usage: 'mcst/mcstatus <server ip>'
 };
 
-const { MessageEmbed, MessageAttachment } = require('discord.js');
+const { MessageEmbed, MessageAttachment, MessageActionRow, Message, MessageButton } = require('discord.js');
 const fetch = require('node-fetch');
 
 module.exports.run = async (client, message, args) => {
-    if (!args[0])return message.reply('You have to include a server ip!');
+    if (!args[0] && !(await client.redis.HEXISTS(`guildSpec_${message.channel.guildId}`, 'default_mc_server')))message.reply('You have to include a server ip!');
     else {
-        fetch(`https://api.mcsrvstat.us/2/${args.join(' ')}`).then(res => res.json()).then(res => {
+        fetch(`https://api.mcsrvstat.us/2/${args.length > 0 ? args.join(' ') : (await client.redis.HGET(`guildSpec_${message.channel.guildId}`, 'default_mc_server'))}`).then(res => res.json()).then(async res => {
             var file = new MessageAttachment('./Images/DefaultMinecraft.png');;
             if (res.ip == "127.0.0.1")return message.reply('That doesn\'t seem to be a valid server. Perhaps you spelled it wrong?')
             else {
@@ -24,7 +24,9 @@ module.exports.run = async (client, message, args) => {
                     if (res.players.list)embed.addField("Players:", res.players.list.join('\n'));
                 }
             } 
-            message.reply({embeds: [embed], files: [file]});
+            await message.reply({embeds: [embed], files: [file], components: [new MessageActionRow().addComponents(new MessageButton().setCustomId(`mcstatus_${args.join(' ')}_all_minecraft`).setLabel('Make Default?').setStyle('SECONDARY').setDisabled(args.length == 0 || ((await client.redis.HGET(`guildSpec_${message.channel.guildId}`, 'default_mc_server')) && (await client.redis.HGET(`guildSpec_${message.channel.guildId}`, 'default_mc_server')) == args.join(' '))))]});
         });
     }
 }
+
+module.exports.button = (client, interaction) => client.redis.HSET(`guildSpec_${interaction.channel.guildId}`, 'default_mc_server', interaction.customId.split('_')[1]);

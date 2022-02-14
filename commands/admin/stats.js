59f517fe5de1c66e3fa7124f457be1f9e61ac127
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2022-02-13 19:25:39 -0500

    removed admin commands from stats

--- commands/admin/stats.js
+++ commands/admin/stats.js
@@ -11,6 +11,6 @@ export const info = {
 export function run(client, message, args) {
     const sort = Array.from(client.stats).sort((a, b) => b[1] - a[1]);
     message.reply({
-        embeds: [ new MessageEmbed().setDescription(sort.map(stat => 't!' + stat[0] + ' ' + client.barCreate((stat[1] / sort[0][1]) * 100) + ' ' + stat[1]).join('\n'))]
+        embeds: [ new MessageEmbed().setDescription(sort.map(stat => 't!' + stat[0] + ' ' + client.barCreate((stat[1] / sort[0][1]) * 100) + ' ' + stat[1]).join('\n') + '\u200b')]
     });
 }

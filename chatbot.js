84d6542968c94d4087ee88559939a14990243b50
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2022-02-16 21:49:30 -0500

    no ping

--- chatbot.js
+++ chatbot.js
@@ -3,5 +3,5 @@ import fetch from 'node-fetch';
 export async function chat(client, message, args) {
     message.channel.sendTyping();
     fetch('http://api.brainshop.ai/get?bid=163846&key=0dGgxyMT9uDkYinn&uid=' + message.author.id + '&msg=' + message.content)
-        .then(res => res.json()).then(res => setTimeout(() => message.reply(res.cnt), 10*res.cnt.length));
+        .then(res => res.json()).then(res => setTimeout(() => message.channel.send(res.cnt), 10*res.cnt.length));
 }

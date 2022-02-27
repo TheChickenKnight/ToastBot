bf114ccbefd294e32c32a284c84728436914d537
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2022-02-27 13:25:39 -0500

    regular imports stop code from working?

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -10,6 +10,8 @@ import { MessageEmbed } from "discord.js";
 
 export function run(client, message, args) {
     const queue = client.queues.get(message.guild.id);
+    if (!queue)
+        return client.error(message, 'there\'s currently no songs playing.');
     client.queues.set(message.guild.id, { loop: !queue.loop, pos: queue.pos, queue: queue.queue });
     message.reply({content: client.tips(), embeds: [new MessageEmbed().setColor('GREEN').setDescription(queue.loop ? 'stopped the current loop!' : 'looping the current queue!')]});
 };

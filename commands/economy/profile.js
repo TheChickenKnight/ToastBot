8fb4d60c0758f96fa112783bbb6ac3e1b9bd1f0c
TheChickenKnight <81781990+TheChickenKnight@users.noreply.github.com>
2022-02-28 06:26:06 -0500

    Economier class, border

--- commands/economy/profile.js
+++ commands/economy/profile.js
@@ -9,10 +9,18 @@ export const info = {
 
 import { MessageEmbed } from "discord.js";
 import { Economier } from "../classes/Economier.js";
+import * as lwip from "lwip";
+
+const border_width = 10;
 
 export async function run(client, message, args) {
-    const obj = await client.redis.get(`economy_${message.author.id}`);
-    const user = new Economier(obj ? obj : {});
+    const user = new Economier(await client.redis.get(`economy_${message.author.id}`) || {});
+    let pfp = await Jimp.read(message.member.displayAvatarURL({ type: 'png' }));
+    new Jimp(pfp.getWidth(), pfp.getHeight(), (err, image) => {
+        if (err)
+            console.error(err);
+        
+    });
     await message.reply({
         embeds: [
             new MessageEmbed()

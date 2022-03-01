cec625b183c1e0bf9db26923051da306e0a7431f
TheChickenKnight <chcknknight@gmail.com>
2022-02-28 21:43:00 -0500

    found working library

--- commands/economy/profile.js
+++ commands/economy/profile.js
@@ -9,18 +9,12 @@ export const info = {
 
 import { MessageEmbed } from "discord.js";
 import { Economier } from "../classes/Economier.js";
-import * as lwip from "lwip";
 
 const border_width = 10;
 
 export async function run(client, message, args) {
     const user = new Economier(await client.redis.get(`economy_${message.author.id}`) || {});
-    let pfp = await Jimp.read(message.member.displayAvatarURL({ type: 'png' }));
-    new Jimp(pfp.getWidth(), pfp.getHeight(), (err, image) => {
-        if (err)
-            console.error(err);
-        
-    });
+    
     await message.reply({
         embeds: [
             new MessageEmbed()
